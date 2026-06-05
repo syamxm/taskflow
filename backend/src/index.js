285f69ec5601f539a1aed2c834d37997ffc6b375
syamxm <ahmadsyamim200@gmail.com>
2026-06-05 19:45:17 +0800

    change max attempts to only 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '10kb' }));
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20,
+  max: 5,
   standardHeaders: true,
   legacyHeaders: false,
   message: { message: 'Too many attempts, try again later' },
