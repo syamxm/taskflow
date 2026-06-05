58e074082d9fe783db0cc6d415b376bef149484d
syamxm <ahmadsyamim200@gmail.com>
2026-06-05 23:31:34 +0800

    Finish cookie auth migration and add CSP

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,12 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: '/api',
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  withCredentials: true,
 });
 
 export default api;
